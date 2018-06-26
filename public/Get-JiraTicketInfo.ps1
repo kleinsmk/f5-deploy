afa6e7d47d4871b691bbbab00bb0b5cd9cc3d133
Klein <551479@bah.com>
2018-06-26 13:15:52 -0400

    Adding Regex subnet and Account validation

--- public/Get-JiraTicketInfo.ps1
+++ public/Get-JiraTicketInfo.ps1
@@ -49,11 +49,28 @@
                 #remove end of line characters etc
                 $awsGroup = $awsGroup -replace "`t|`n|`r",""
                 $subnet = $subnet -replace "`t|`n|`r",""
-                               
-                $envInfo =  [PSCustomObject]@{
-	                'cr' = $crNumber
-	                'aws_group' = $awsGroup[1]
-	                'subnet' = $subnet[1]
+                
+                #match for AWS account like AWS_293853093962 or Azure MAZ_8ccd4180-a8b6-413e-870f-b50af1e0647b
+                if($awsGroup[1] -match '^AWS_[0-9]*$|^MAZ_[a-zA-Z0-9]*-[a-zA-Z0-9]*-[a-zA-Z0-9]*-[a-zA-Z0-9]*-[a-zA-Z0-9]*$'){
+                    #match for CIDR like 10.194.83.192/26           
+                    if( $subnet[1] -match '^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$' ){
+                    
+                        $envInfo =  [PSCustomObject]@{
+	                    'cr' = $crNumber
+	                    'aws_group' = $awsGroup[1]
+	                    'subnet' = $subnet[1]
+                        }
+
+                        
+                    }
+                    else { Write-Error "Jira ticket has malforned User Private Subnet  info that cannot be scraped. Check for newline characters etc" -ErrorAction Stop }
+                 
+                }
+                    
+
+                else {
+                        
+                        Write-Error "Jira ticket has malforned AWS or Azure account info that cannot be scraped. Check for newline characters etc" -ErrorAction Stop
                 }
 
                 $envInfo
