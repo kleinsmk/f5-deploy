0f2fae293218b2ac756dd62a09325e1975060e5e
Klein <551479@bah.com>
2018-06-26 13:48:15 -0400

    Removed non module files

--- public/Create New Stack Dev.ps1
+++
@@ -1,7 +0,0 @@
-﻿Connect-F5 -ip 10.219.1.183
-
-New-DefaultAcl -name "Rich" -subnet "10.194.57.192/26"
-
-Add-APMRole -name "aggregate_acl_act_full_resource_assign_ag" -acl "Rich" -group "Rich_awsgroup"
-
-Update-APMPolicy -name "CSN_VPN_Streamlined"
