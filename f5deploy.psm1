da8ae50580e47ad6bfd7f3afe6d04b36e13cc49e
Klein <551479@bah.com>
2018-06-26 09:46:44 -0400

    new Dev bracnh and start of modules

---
+++ f5deploy.psm1
@@ -0,0 +1,29 @@
+<#
+.SYNOPSIS  
+    Load Core Services Helper CMDlets
+.DESCRIPTION  
+    This module uses the F5 LTM REST API to manipulate and query
+.NOTES  
+    File Name    : deploy.psm1
+    Author       : Sky Klein
+    Requires     : 
+    Dependencies : Requires F5-LTM module for connections to F5
+#>
+
+
+
+
+$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
+#region Load Public Functions
+
+    Get-ChildItem "$ScriptPath\Public" -Filter *.ps1 -Recurse| Select-Object -Expand FullName | ForEach-Object {
+        $Function = Split-Path $_ -Leaf
+        try {
+            . $_
+        } catch {
+            Write-Warning ("{0}: {1}" -f $Function,$_.Exception.Message)
+        }
+   }
+
+#endregion
+
