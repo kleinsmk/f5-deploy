01598ef8649f7a06124b06c0d888ce9e9a61e150
Klein <551479@bah.com>
2018-06-29 11:24:37 -0400

    adding Get AsmPolicies function

---
+++ public/Get-AsmPolicies.ps1
@@ -0,0 +1,53 @@
+﻿Function Get-ASMPolicies {
+
+    [cmdletBinding()]
+    param(
+        
+        [Alias("acl Name")]
+        [Parameter(Mandatory=$false)]
+        [string[]]$name=''
+
+    )
+
+     begin {
+        #Test that the F5 session is in a valid format
+        Test-F5Session($F5Session)
+    }
+
+
+    Process {
+
+             
+             $uri = $F5Session.BaseURL.Replace('/ltm/',"/asm/policies") 
+
+            try {
+
+                    if ([string]::IsNullOrEmpty($name)) {
+                        $response = Invoke-RestMethodOverride -Method GET -Uri $URI -WebSession $F5Session.WebSession
+                        $response 
+                    }
+                    
+                    else{
+                    
+                        $response = Invoke-RestMethodOverride -Method GET -Uri $URI -WebSession $F5Session.WebSession
+                        $response | Select-Object -ExpandProperty items | where {$_.name -eq $name}
+                    
+                    }               
+
+
+                }
+
+            catch {
+
+                $message = $Error[0].ErrorDetails.Message | ConvertFrom-Json
+                Write-Host $message.message
+
+            }
+        }
+}
+
+    
+
+  
+
+
