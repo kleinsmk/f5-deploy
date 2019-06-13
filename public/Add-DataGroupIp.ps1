04a3763e559e3d1a0958c3c26ea86c59acbcfa83
Sky <kleinsmk@gmail.com>
2019-06-13 15:39:45 -0400

    adding new functions

---
+++ public/Add-DataGroupIp.ps1
@@ -0,0 +1,50 @@
+Function Add-DataGroupIp {
+<#
+.SYNOPSIS
+    Adds a new data pair to a DataGroup IP
+.DESCRIPTION
+    Adds a new data pair to a DataGroup IP
+.PARAMETER virtual
+    Virtual server name to append to
+.PARAMETER profile
+    Profile to append to collection
+.NOTES
+    Requires F5-LTM modules from github
+.EXAMPLE
+    Add-DataGroupIP
+#>
+    [cmdletBinding()]
+    param(
+        
+
+        [Parameter(Mandatory=$true)]
+        [string]$groupName,
+
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName)]
+        [string[]]$address,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName)]
+        [string[]]$value
+
+    )
+    begin {
+        #check if session is active or else break
+        Check-F5Token   
+    }
+    process {
+       
+        $json = "{`"name`":`"$groupName`"}"
+
+        foreach ($ip in $address) {
+
+            foreach ($item in $value){
+
+                $uri = $F5Session.BaseURL.Replace('/ltm/',"/ltm/data-group/internal/$groupName" + "?options=records+add+{$ip{data+`"$item`"}}")
+                Write-Debug "Adding $uri"
+                Invoke-RestMethodOverride -Method PATCH -URI $uri -WebSession $F5Session.WebSession -ContentType 'application/json' -Body $json
+            }
+        }
+   }
+        
+}
