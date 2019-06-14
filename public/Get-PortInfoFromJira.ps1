b6a4788019d48645b46ca1be4f44c58fa4d4ca2f
Sky Klein <sklein@csn.internal>
2019-06-14 16:15:43 -0400

    Fix bug with hidden control characters

--- public/Get-PortInfoFromJira.ps1
+++ public/Get-PortInfoFromJira.ps1
@@ -40,6 +40,9 @@
                     #split description into an array of lines that match the input pattern
                     $desc = $desc | Select-String -Pattern $pattern
                     
+                    #remove end of line characters etc
+                    $desc = $desc -replace "`t|`n|`r",""
+
                     #loop through the array an return a custom object of arrays of 
                     foreach ($item in $desc){
 
