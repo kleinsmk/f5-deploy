0c38d1397ca951a7a0fbfd3e6696bf962a7ae855
Sky <kleinsmk@gmail.com>
2019-07-03 09:14:30 -0400

    added more tests

---
+++ Tests/New-AwsSecurityStack.Tests.ps1
@@ -0,0 +1,64 @@
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "..\public\$sut"
+
+
+
+Describe "New-AwsSecurityStack AWS Deployment Integration Test" {
+
+    Mock Get-JiraTicketInfo {$newEnv}
+
+    Mock Get-NextAclOrder {9000}
+
+    Mock Update-APMPolicy {}
+
+    Mock Sync-DeviceToGroup {}
+
+    Mock Add-JiraIssueComment {}
+
+    Mock Invoke-JiraIssueTransition {}
+
+    Mock Get-JiraIssue {}
+
+    BeforeAll {
+        $newEnv = @{"cr" = "CR-1000"; "aws_group" = "AWS_00000000000"; "subnet" = "1.2.3.4/32"}
+    }
+
+    New-AwsSecurityStack -crnumber $newEnv.cr -f5creds $f5 -jiracreds $jira
+
+    Context "Checking On-Premise" {
+    
+        Connect-F5 -ip op -creds $f5
+
+        It "Created an On-Premise Acl With Default Entries" {
+            $test = Get-SingleAcl -name $newEnv.aws_group
+            $test.entries | Should not be $null   
+        }
+
+        It "Created an APM Role Mapping On-Premise" {
+            $test = Get-APMRole -name acl_1_act_full_resource_assign_ag 
+            $test = $test | Where-Object { $_.acls -eq "/Common/$($newEnv.aws_group)" }
+            $test | Should not be $null
+
+        }
+        
+    }
+
+    Context "Checking AWS" {
+    
+        Connect-F5 -ip aws -creds $f5
+
+        It "Created an On-Premise Acl With Default Entries" {
+            $test = Get-SingleAcl -name $newEnv.aws_group
+            $test.entries | Should not be $null   
+        }
+
+        It "Created an APM Role Mapping On-Premise" {
+            $test = Get-APMRole -name acl_1_act_full_resource_assign_ag 
+            $test = $test | Where-Object { $_.acls -eq "/Common/$($newEnv.aws_group)" }
+            $test | Should not be $null
+
+        }
+        
+    }
+    
+}
