37318c31a1ca6854f3751b0c7c75bcbae1608620
Sky <kleinsmk@gmail.com>
2019-07-08 17:44:52 -0400

    Fix case typo

--- Tests/Get-NodebyFQDN.Tests.ps1
+++ Tests/Get-NodebyFQDN.Tests.ps1
@@ -1,6 +1,5 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "..\public\$sut"
+. "../public/$sut"
 
 $mockJson = @"
 {
