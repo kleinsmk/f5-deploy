0422aa6c33942c8eee465876c0a6872f2ca499d7
Sky <kleinsmk@gmail.com>
2019-07-09 11:57:53 -0400

    Invoke-Asm passing tests

--- Tests/Invoke-AsmTask.Tests.ps1
+++ Tests/Invoke-AsmTask.Tests.ps1
@@ -1,8 +1,10 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "../public/$sut"
 
-Context "Function Unit Tests" {
-   Describe "Invoke-AsmTask" -Tag Unit {
+Context "Function Unit Tests" -Tag "Unit" {
+   Describe "Invoke-AsmTask"  {
+
+    $json = "{'test':'test'}"
 
     Mock Invoke-RestMethodOverride {
 
@@ -12,8 +14,42 @@ Context "Function Unit Tests" {
       #Mundane API call test to check for a return
       It "Returns an Object" {
          
-         $result = Invoke-AsmTask -json $json
+         $result = Invoke-AsmTask -task $json
          $result | Should -Not -Be $null
       }       
    }
+}
+
+Context "Function Integration Tests" -Tag "Integration"{
+    Describe "Connecting to F5 for Testing" {
+        #need to have credentials stored for user in f5 cred manager
+        It "Connecitng to F5 for testing" {
+            {Connect-F5 -ip ec2f5.boozallencsn.com -creds $f5} | Should Not Throw
+        }
+
+        It "Starts a new ASM Task " {
+
+                $json = @"
+                {
+                    "commands": [
+                    {
+                        "uri": "https://aws/mgmt/tm/asm/policies/cR2ICBCueib6eZmArrKDrA",
+                        "body": {
+                        "kind": "tm:asm:policies:policystate",
+                        "virtualServers": [
+                            "/Common/integration-testing"
+                        ]
+                        },
+                        "method": "PATCH"
+                    }
+                    ]
+                }
+"@
+
+            $task = Invoke-AsmTask -task $json 
+            $task.Status | Should Be "NEW"
+
+        }
+    }
+
 }
