fd4bc74648c849bc975cf4de27f3865b872a33d2
Sky <kleinsmk@gmail.com>
2019-07-15 12:34:13 -0400

    Adding task functions

---
+++ public/Get-AsmApplyPolicyStatus.ps1
@@ -0,0 +1,22 @@
+Function Get-AsmApplyPolicyStatus {
+   <#
+   .SYNOPSIS
+       Gets the status of all ASM apply policy tasks
+   .Description
+       Gets the status of all ASM apply policy tasks
+   .EXAMPLE
+        GGet-AsmApplyPolicyStatus
+   .NOTES
+      
+       Requires F5-LTM modules from github
+   #>
+
+       process { 
+
+               $uri = $F5Session.BaseURL.Replace('/ltm/','/asm/tasks/apply-policy') 
+               $response = Invoke-RestMethodOverride -Method Get -Uri $URI -ContentType 'application/json' -WebSession $F5Session.WebSession
+               $response.Status
+            }
+       }
+}
+   
---
+++ public/Start-ApplyAllAsmPolicies.ps1
@@ -0,0 +1,43 @@
+Function Start-ApplyAllAsmPolicies {
+  <#
+
+  .DESCRIPTION
+      Creates and apply policy task for each existing ASM policy in the F5
+  .NOTES
+      Requires F5-LTM modules from github
+  .EXAMPLE
+      Start-ApplyAllAsmPolicies
+  #>
+
+      begin {
+          #check if session is active or else break
+          Check-F5Token   
+      }
+      process {
+         
+          $policy = Get-ASMPolicies
+
+          foreach ($selflink in $policy.items.selflink){ 
+
+          $array += @"
+{
+     "policyReference":
+     {
+       "link": "$selflink"
+   
+     }
+   }
+"@
+          } 
+  
+          $uri = $F5Session.BaseURL.Replace('/ltm/','/asm/tasks/apply-policy')
+
+          foreach ($item in $array) {               
+
+                  Invoke-RestMethodOverride -Method POST -URI $uri -Body $item -ContentType "application/json" -WebSession $F5Session.WebSession 
+          }
+     }
+          
+  }
+  
+
