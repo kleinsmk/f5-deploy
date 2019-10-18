67c9fa395c2a05f8ced1a620ad8b1ace6e1f78c8
Sky <kleinsmk@gmail.com>
2019-10-18 10:15:27 -0400

    saving changes

--- Remove-Project.ps1
+++ Remove-Project.ps1
@@ -27,40 +27,62 @@ Function Remove-OldProject {
                
                 $virtuals = Get-VirtualServer
 
+                #filter for virtual object with matching pool    
                 $vs = $virtuals | Where-Object { $_.pool -eq "$pool" }
 
-                foreach ($rule in $vs.rules){
-
-                    Write-Host "Remove-iRuleFromVirtualServer -Name '$($vs.name)' -iRuleName '$rule' "
-                    if ($rule -ne "/Common/BAH_Hide_NAT_Source_Restriction") {
-                        Write-Host "Remove-iRule -Name '$rule' -Confirm:`$false"
+                #are there any irules present?
+                if( !([string]::IsNullOrEmpty($vs.rules)) ) {
+                    foreach ($rule in $vs.rules){
+                        #logic to remove irules from vs NEED TO MAKE SURE RULE IS OKAY TO REMOVE
+                        Write-Host "Remove-iRuleFromVirtualServer -Name '$($vs.name)' -iRuleName '$rule' "
+                        if ($rule -ne "/Common/BAH_Hide_NAT_Source_Restriction") {
+                            Write-Host "Remove-iRule -Name '$rule' -Confirm:`$false"
+                        }
+                        
                     }
-                    
                 }
 
+                else { Write-Host "No iRules were attached to the server."}
+
 ##############################Profile Removal Function#############################
-                #SHOULD REDO THIS SO THAT IS FILTERS EXISTING VS PULL TO SAVE TIME
-                $vsProfile =  $vs.name
 
                 #grab all profiles reference links that have ssl and are therefore ssl client or server profiles    
                 $profiles = $vs.profilesReference.items.namereference.link | Where-Object {$_ -match 'ssl'}
+                
                 #text processing to create an object with only the profile names
                 $profiles = $profiles | ForEach-Object { ( $_.split('~Common~')[1] ).split('?')[0] }
-
+                
+                #filter profiles by type since we need to know to remove them
                 $serverProfiles = $profiles -match 'server'
                 $clientProfiles =  $profiles -match 'client'
 
-                Write-Host "Remove-SSL client $clienprofiles"
+                #code to get profiles in order to grab key and profile info
+
+                #code to detach an ssl profile first
+
+                #code to get / delete key
+
+                #code to get / delete cert
+                
+                #Code to then delte ssl profiles
+                Write-Host "Remove-SSL client $clientprofiles"
                 Write-Host "Remove-SSLServer $serverProfiles "
 
 #################################################                
 
+                #Code to Remove Virtual Server
                 Write-Host "Remove-VirtualServer -Name $($vs.name) -Confirm:`$false"
+
+                #Code to Remove Pool
                 Write-Host "Remove-Pool -PoolName $pool -Confirm:`$false"
 
+                #API call to grab pool members based on pool name
                 $member = Get-PoolMember -PoolName $pool
+
+                #Trim port info off end of pool member name
                 $member = $member.name.TrimEnd(":") 
 
+                #Commands to remove Node and ASM
                 Write-Host "Remove-Node -Name $member -Confirm:`$false"
                 Write-Host "Remove-Asm -policyname $($vs.policiesReference.items.name) -Confirm:`$false"
             }
