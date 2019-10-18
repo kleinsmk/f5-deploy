948a84bc28d162cc8d23495a1a308111f70a322d
Sky <kleinsmk@gmail.com>
2019-10-18 10:30:14 -0400

    adding new function

---
+++ public/Start-Transaction.ps1
@@ -0,0 +1,29 @@
+Function Start-Transaction {
+   <#
+   .SYNOPSIS
+       Starts ASM Transaction logging on F5 Load Balancer
+   .Description
+       Starts ASM Transaction logging on F5 Load Balancer.  This will capture all POST requests to be batch deployed
+       via the transaction model.  This model only executes the changes if it determines all subtasks will complete.
+   .EXAMPLE
+       Start-Transaction
+   .NOTES
+      
+       Requires F5-LTM modules from github
+   #>
+       [cmdletBinding()]
+       param(
+   
+       )
+
+       process {        
+
+               $uri = $F5Session.BaseURL.Replace('/ltm/','/transaction') 
+               $response = Invoke-RestMethodOverride -Method Post -Uri $URI -Body $item -ContentType 'application/json' -WebSession $F5Session.WebSession
+        
+               #Add headers to existing F5 Session to caputre POSTS as transactions
+               $F5Session.WebSession.Headers.Add("X-F5-REST-Coordination-Id",$($response.transId))
+            
+       }
+}
+   
